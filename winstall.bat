48cc2b163a9834dbbf68bec9c881ac68246c1afc
Justin Russell <jus.russell@gmail.com>
2016-12-29 18:59:56 -0500

    Get the latest version of Node.js

--- winstall.bat
+++ winstall.bat
@@ -3,3 +3,8 @@ REM Get chocolatey
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 choco install packages.confg -y
+
+%ProgramData%\chocolatey\bin\RefreshEnv.cmd
+
+nvm install latest
+nvm use latest
