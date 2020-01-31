26f4d3b6cb050403bca284b0355e5a2a59c3937c
Evan Sheffield <evan.c.sheffield@gmail.com>
2020-01-31 12:21:37 -0600

    fix: Fix typo in name of choco packages file

--- winstall.bat
+++ winstall.bat
@@ -3,7 +3,7 @@ REM Get chocolatey
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 REM Install da packages
-choco install packages.confg -y
+choco install packages.config -y
 
 REM Get the latest node
 REM Note that `nvm install latest` works but `nvm use latest` does not. Hence
